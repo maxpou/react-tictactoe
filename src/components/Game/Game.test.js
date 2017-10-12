90dead2cf9e4672b0ed5edd8a0652650499c76d9
Maxence POUTORD <maxence.poutord@gmail.com>
2017-10-12 13:07:22 +0100

    :white_check_mark: add tests

--- src/components/Game/Game.test.js
+++ src/components/Game/Game.test.js
@@ -1,10 +1,40 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow, mount } from 'enzyme'
 import Game from './Game'
 
 describe('Game.js', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<Game />, div)
+    const wrapper = shallow(<Game />)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('Same player should not play 2 times in a row', () => {
+    const wrapper = mount(<Game />)
+    const firstPlayer = wrapper.find('div.game-info').children().first().text()
+    expect(firstPlayer).toEqual('Next player: X')
+
+    const button = wrapper.find('button.square').first()
+    button.simulate('click')
+    const secondPlayer = wrapper.find('div.game-info').children().first().text()
+    expect(secondPlayer).toEqual('Next player: O')
+  })
+
+  it('should declare a winner', () => {
+    const wrapper = mount(<Game />)
+
+    const round1 = wrapper.find('button.square').at(0)
+    round1.simulate('click')
+    const round2 = wrapper.find('button.square').at(1)
+    round2.simulate('click')
+    const round3 = wrapper.find('button.square').at(3)
+    round3.simulate('click')
+    const round4 = wrapper.find('button.square').at(2)
+    round4.simulate('click')
+    const round5 = wrapper.find('button.square').at(6)
+    round5.simulate('click')
+
+    const winnerDeclaration = wrapper.find('.game-info').children().first().text()
+    console.log(winnerDeclaration)
+    expect(winnerDeclaration).toEqual('Winner: X')
   })
 })
